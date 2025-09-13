538182d6e717883e4c96bbb5b43cc3371406d7f0
srujaniyengar <srujanparthasarathyiyengar@gmail.com>
2025-09-13 18:32:37 +0530

    add: urls and serialization

---
+++ backend/src/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'full_name', 'role', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            full_name=validated_data['full_name'],
+            role=validated_data['role'],
+            password=validated_data['password']
+        )
+        return user
