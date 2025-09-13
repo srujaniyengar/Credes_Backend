a033405e7c30f6c5e9eb9a2a604ea90830c0cef6
srujaniyengar <srujanparthasarathyiyengar@gmail.com>
2025-09-13 20:06:58 +0530

    mod settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,9 +123,15 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "src.User"
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
+SIMPLE_JWT = {
+
 }
